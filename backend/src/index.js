9d7b65eb3f64b49376de4116e3f1c9889ca2751f
Akash Purohit <akashpurohit1235@gmail.com>
2025-05-03 22:19:37 +0530

    code with authentication system ready

---
+++ backend/src/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
+
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+const app=express();
+
+const PORT=process.env.PORT;
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+})
+)
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+
+app.use("/api/auth",authRoutes);
+app.use("/api/message",messageRoutes);
+
+
+app.listen(PORT,()=>{
+    console.log("server is running on port:"+ PORT);
+    connectDB();
+}); 
