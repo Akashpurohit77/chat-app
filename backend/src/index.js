416a6576f97b93a621d87b6f0709807e23e86fee
Akash Purohit <akashpurohit1235@gmail.com>
2025-06-28 03:05:39 +0530

    fixed the main route

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,9 +25,9 @@ app.use(cors({
 })
 )
 
-app.get("/", (req, res) => {
-    res.send("API is running...");
-});
+// app.get("/", (req, res) => {
+//     res.send("API is running...");
+// });
     
 
 app.use("/api/auth", authRoutes);
